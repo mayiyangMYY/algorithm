acc21242deadf41824f61d21d4953059bea282b9
马熠阳 <41225592+wlmayiyang@users.noreply.github.com>
2018-12-15 17:14:11 +0800

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,36 +1,59 @@
+单点修改
+int n,a[N],mx[N<<2],sum[N<<2];
 void push_up(int x)
 {
-    mx[x]=max(mx[x+x],mx[x+x+1]);
+	sum[x]=sum[x+x]+sum[x+x+1];
+	mx[x]=max(mx[x+x],mx[x+x+1]);
 }
+
 void build(int x,int l,int r)
 {
-    if (l==r)
-    {
-        mx[x]=a[l];
-        return;
-    }
-    int m=(l+r)>>1;
-    build(x+x,l,m);
-    build(x+x+1,m+1,r);
-    push_up(x);
+	if (l==r)
+	{
+		mx[x]=a[l];
+		sum[x]=a[l];
+		return;
+	}
+	int m=(l+r)>>1;
+	build(x+x,l,m);
+	build(x+x+1,m+1,r);
+	push_up(x);
 }
-void modify(int x,int l,int r,int pos,int val)
+
+void modify(int x,int l,int r,int pos,int num)
 {
-    if (l==r)
-    {
-        mx[x]=val;
-        return;
-    }
-    int m=(l+r)>>1;
-    if (pos<=m) modify(x+x,l,m,pos,val);
-    else modify(x+x+1,m+1,r,pos,val);
-    push_up(x);
+	if (l==r)
+	{
+		sum[x]=num;
+		mx[x]=num;
+		return;
+	}
+	int m=(l+r)>>1;
+	if (pos<=m) modify(x+x,l,m,pos,num);
+	else modify(x+x+1,m+1,r,pos,num);
+	push_up(x);
 }
-int query(int x,int l,int r,int L,int R)
+
+int query_max(int x,int l,int r,int L,int R)
 {
-    int m=(l+r)>>1,ans=0;
-    if (L<=l&&r<=R) return mx[x];
-    if (L<=m) ans=max(ans,query(x+x,l,m,L,R));
-    if (R>m) ans=max(ans,query(x+x+1,m+1,r,L,R));
-    return ans;
+	if (L<=l&&r<=R)
+	{
+		return mx[x];
+	}
+	int m=(l+r)>>1,ans=-inf;
+	if (L<=m) ans=max(ans,query_max(x+x,l,m,L,R));
+	if (R>m) ans=max(ans,query_max(x+x+1,m+1,r,L,R));
+	return ans;
+}
+
+int query_sum(int x,int l,int r,int L,int R)
+{
+	if (L<=l&&r<=R)
+	{
+		return sum[x];
+	}
+	int m=(l+r)>>1,ans=0;
+	if (L<=m) ans+=query_sum(x+x,l,m,L,R);
+	if (R>m) ans+=query_sum(x+x+1,m+1,r,L,R);
+	return ans;
 }
