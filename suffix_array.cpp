02a4c214ca52d45e070d65ac81adc065b6b835db
马熠阳 <41225592+wlmayiyang@users.noreply.github.com>
2020-11-26 20:16:07 +0800

    Update suffix_array.cpp

--- suffix_array.cpp
+++ suffix_array.cpp
@@ -1,54 +1,39 @@
-char s[N];
-int n,Rnk[N<<1],cnt[N],P[N],Q[N],sa[N],height[N];
-void get_sa()
+namespace SA
 {
-    for (int i=0;i<=200;++i) cnt[i]=0;
-    for (int i=1;i<=n;++i) cnt[s[i]]=1;
-    for (int i=1;i<=200;++i) cnt[i]+=cnt[i-1];
-    for (int i=1;i<=n;++i) Rnk[i]=cnt[s[i]];
-    for (int now=1;now<n;now<<=1)
-    {
-        for (int i=0;i<=n;++i) cnt[i]=0;
-        cnt[0]=now;
-        for (int i=now+1;i<=n;++i) ++cnt[Rnk[i]];
-        for (int i=1;i<=n;++i) cnt[i]+=cnt[i-1];
-        for (int i=n;i>=1;--i)
-        {
-            int tmp=Rnk[i+now];
-            P[cnt[tmp]]=i; --cnt[tmp];
-        } 
-        for (int i=0;i<=n;++i) cnt[i]=0;
-        for (int i=1;i<=n;++i) ++cnt[Rnk[P[i]]];
-        for (int i=1;i<=n;++i) cnt[i]+=cnt[i-1];
-        for (int i=n;i>=1;--i)
-        {
-            Q[cnt[Rnk[P[i]]]]=P[i];
-            --cnt[Rnk[P[i]]];
-        }
-        int tmp=0;
-        for (int i=1;i<=n;++i)
-        {
-            if (Rnk[Q[i]]!=Rnk[Q[i-1]]||Rnk[Q[i]+now]!=Rnk[Q[i-1]+now]) ++tmp;
-            P[i]=tmp;
-        }
-        for (int i=1;i<=n;++i) Rnk[Q[i]]=P[i];
-        if (tmp==n) break;
-    }
-    for (int i=1;i<=n;++i) sa[Rnk[i]]=i;
-    for (int i=1;i<=n;++i) printf("%d%c",sa[i]," \n"[i==n]);
-}
-
-//height[i] -> the length of LCP of SA[i-1] and SA[i]
-//h[i]=height[Rnk[i]]   h[i]>=h[i-1]-1
-//LCP(suffix i,suffix j)=RMQ(height,rnk[i]+1,rnk[j]);
-void get_height()
-{
-    int tmp=0;
-    for (int i=1;i<=n;++i)
-    {
-        if (tmp) --tmp;
-        int j=sa[Rnk[i]-1];
-        while (s[j+tmp]==s[i+tmp]) ++tmp;
-        height[Rnk[i]]=tmp;
-    }
+	int n,alpha,top;
+	int cnt[N],sa[N],rnk[N],rnk2[N],height[N];
+	void build(char *s)
+	{
+		alpha=130;
+		n=strlen(s+1);
+		for (int i=1;i<=n;++i) ++cnt[s[i]],rnk[i]=s[i];
+		for (int i=2;i<=alpha;++i) cnt[i]+=cnt[i-1];
+		for (int i=n;i>=1;--i) sa[cnt[rnk[i]]--]=i;
+		for (int len=1;len<=n;len<<=1)
+		{
+			top=0;
+			for (int i=n-len+1;i<=n;++i) rnk2[++top]=i;
+			for (int i=1;i<=n;++i) if (len<sa[i]) rnk2[++top]=sa[i]-len;
+			memset(cnt,0,sizeof(int)*(alpha+1));
+			for (int i=1;i<=n;++i) ++cnt[rnk[i]];
+			for (int i=2;i<=alpha;++i) cnt[i]+=cnt[i-1];
+			for (int i=n;i>=1;--i) sa[cnt[rnk[rnk2[i]]]--]=rnk2[i];
+			alpha=0;
+			for (int i=1;i<=n;++i)
+			{
+				if ((rnk[sa[i-1]]^rnk[sa[i]])||(rnk[sa[i-1]+len]^rnk[sa[i]+len])) ++alpha;
+				rnk2[sa[i]]=alpha;
+			}
+			memcpy(rnk,rnk2,sizeof(int)*(n+1));
+			if (alpha==n) break;
+		}
+		int tmp=0;
+		for (int i=1;i<=n;++i)
+		{
+			if (tmp) --tmp;
+			int j=sa[rnk[i]-1];
+			while (s[i+tmp]==s[j+tmp]) ++tmp;
+			height[rnk[i]]=tmp;
+		}
+	}
 }
